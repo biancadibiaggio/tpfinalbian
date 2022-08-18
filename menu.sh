48f362ae6be54cf156846304bdde82c57e19fc04
biancadibiaggio4423@gmail.com <biancadibiaggio4423@gmail.com>
2022-08-18 10:32:19 -0300

    menu corregido

--- menu.sh
+++ menu.sh
@@ -10,18 +10,19 @@ PATH_ARCHIVO=texto.txt
 
 PS3='=> '
 echo "Elija una opcion"
-select opcion in "SALIR" "Estadisticas_de_palabras" "Estadisticas_de_uso_de_palabras" "Encontrar_nombres" "Estadisticas_de_oraciones" "Contador_de_lineas en blanco"
+select opcion in "Estadísticas_de_palabras" "Estadísticas_de_uso_de_palabras" "Encontrar_nombres_propios" "Estadísticas_de_oraciones" "Contador_de_lineas_en_blanco" "Salir"
 do
         [ -z $opcion ] && echo "Elija una opcion" && continue
-        [ $REPLY == 1 ] && echo "Saliendo..." && break
-        echo "Opcion elegida: " $opcion
+        [ $REPLY == 6 ] && echo "Saliendo, vuelva prontos" && break
+        echo "La opción de filtrado eligida es: " $opcion
+        echo "Resultados:"
 
         case $REPLY in 
-          2) 1statsWords $PATH_ARCHIVO;;
-          3) 2statsUsageWords $PATH_ARCHIVO;;
-          4) 3findNames $PATH_ARCHIVO;;
-          5) 4statsSentences $PATH_ARCHIVO;;
-          6) 5blankLinesCounter $PATH_ARCHIVO;;
+          1) 1statsWords $PATH_ARCHIVO;;
+          2) 2statsUsageWords $PATH_ARCHIVO;;
+          3) 3findNames $PATH_ARCHIVO;;
+          4) 4statsSentences $PATH_ARCHIVO;;
+          5) 5blankLinesCounter $PATH_ARCHIVO;;
         esac
 done
 
