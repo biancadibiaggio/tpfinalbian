9054c6bdb476c2c263ceb943ba8ce66e58948148
biancadibiaggio4423@gmail.com <biancadibiaggio4423@gmail.com>
2022-08-19 14:33:01 -0300

    menu listo

--- menu.sh
+++ menu.sh
@@ -12,7 +12,7 @@ PS3='>> '
 echo "Elija una opción"
 select opcion in "Palabras" "Uso_de_palabras" "Nombres" "Oraciones" "Lineas_en_blanco" "SALIR"
 do
-	[ -z $opcion ] && echo "Elija una opción válida" && continue
+	[ -e $opcion ] && echo "Elija una opción válida" && continue
 	[ $REPLY == 6 ] && echo "Hasta luego, vuelva prontos" && break
 	echo "Usted esta viendo estadísticas sobre " $opcion
 	
